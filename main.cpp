d6b58f3aa876777ea03890dcb39512c55fc931c9
chen-qilian <2602852685@qq.com>
2024-02-01 19:41:40 +0800

    单例模式更新

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <thread>
+#include <functional>
+#include <mutex>
+
 using namespace std;
+once_flag g_flag;
 void func()
 {
     cout<<endl;
@@ -13,15 +17,53 @@ void func2(string name ,int age)
     cout<<name<<endl;
     cout<<endl;
 }
+//单例模式---懒汉模式（call_once()实现）
+class Base
+{
+public:
+    Base(const Base& obj) = delete;
+    Base& operator= (const Base& obj) = delete;
+    static Base* getInstance()
+    {
+        call_once(g_flag , [&](){
+            obj  = new Base;
+            cout<<"chuangjian"<<endl;
+        });
+        return obj;
+    }
+
+    void set(string name)
+    {
+        this->name = name;
+    }
+    string getName()
+    {
+        return name;
+    }
+private:
+    Base(){};
+    static  Base* obj;
+    string name;
+};
+
+// 在类定义之后
+Base* Base::obj = nullptr;
 int main() {
-//    thread t1;
+//    //测试多线程
+//    thread t2(func);
+//
+//    thread t3(func2,"my",2);
+//
+//    t2.join();
+//    t3.join();
 
-    thread t2(func);
+    Base::getInstance()->set("ddd");
+    cout<<Base::getInstance()->getName()<<endl;
+//
+    Base::getInstance()->set("qq");
+    cout<<Base::getInstance()->getName()<<endl;
 
-    thread t3(func2,"my",2);
-//    std::cout << "Hello, World!" << std::endl;
-//    t1.join();
-    t2.join();
-    t3.join();
+    Base::getInstance()->set("ccc");
+    cout<<Base::getInstance()->getName()<<endl;
     return 0;
 }
