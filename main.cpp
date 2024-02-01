175341e44dd3efcda98e2edc7402230691e5ed7b
chen-qilian <2602852685@qq.com>
2024-02-01 17:01:48 +0800

    第二次提交

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
-
+#include <thread>
+using namespace std;
+void func()
+{
+    cout<<endl;
+    cout<<"jhHHh"<<endl;
+    cout<<endl;
+}
+void func2(string name ,int age)
+{
+    cout<<endl;
+    cout<<name<<endl;
+    cout<<endl;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    thread t1;
+
+    thread t2(func);
+
+    thread t3(func2,"my",2);
+//    std::cout << "Hello, World!" << std::endl;
+//    t1.join();
+    t2.join();
+    t3.join();
     return 0;
 }
